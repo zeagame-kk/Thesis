41812c45fbbb4994cd023beed76f131a9abe732a
Nuttun <zg.nuttun@gmail.com>
2023-04-15 07:43:43 +0000

    updated

--- helper_train.py
+++ helper_train.py
@@ -117,6 +117,7 @@ def train_autoencoder_v1(num_epochs, model, optimizer, device,
 
             # LOGGING
             log_dict['train_loss_per_batch'].append(loss.item())
+            log_dict['train_acc_per_epoch'].append(train_acc.item())
             
             if not batch_idx % logging_interval:
                 print('Epoch: %03d/%03d | Batch %04d/%04d | Loss: %.4f'
@@ -142,7 +143,104 @@ def train_autoencoder_v1(num_epochs, model, optimizer, device,
     
     return log_dict
 
+# Use this
+def train_vae_v2(num_epochs, model, optimizer, device, 
+                 train_loader, valid_loader=None,
+                 loss_fn=None,
+                 logging_interval=100, 
+                 skip_epoch_stats=False,
+                 reconstruction_term_weight=1,
+                 save_model=None):
+    
+    log_dict = {'train_combined_loss_per_batch': [],
+                'train_combined_loss_per_epoch': [],
+                'train_reconstruction_loss_per_batch': [],
+                'train_kl_loss_per_batch': [],
+                'train_acc_per_epoch': [],       # Add this
+                'valid_acc_per_epoch': [],       # Add this
+                'valid_loss_per_epoch': []}      # Add this
 
+    if loss_fn is None:
+        loss_fn = F.mse_loss
+
+    start_time = time.time()
+    for epoch in range(num_epochs):
+
+        model.train()
+        for batch_idx, (features, _) in enumerate(train_loader):
+
+            features = features.to(device)
+
+            # FORWARD AND BACK PROP
+            encoded, z_mean, z_log_var, decoded = model(features)
+            
+            # total loss = reconstruction loss + KL divergence
+            #kl_divergence = (0.5 * (z_mean**2 + 
+            #                        torch.exp(z_log_var) - z_log_var - 1)).sum()
+            kl_div = -0.5 * torch.sum(1 + z_log_var 
+                                      - z_mean**2 
+                                      - torch.exp(z_log_var), 
+                                      axis=1) # sum over latent dimension
+
+            batchsize = kl_div.size(0)
+            kl_div = kl_div.mean() # average over batch dimension
+    
+            pixelwise = loss_fn(decoded, features, reduction='none')
+            pixelwise = pixelwise.view(batchsize, -1).sum(axis=1) # sum over pixels
+            pixelwise = pixelwise.mean() # average over batch dimension
+            
+            loss = reconstruction_term_weight*pixelwise + kl_div
+            
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            # UPDATE MODEL PARAMETERS
+            optimizer.step()
+
+            # LOGGING
+            log_dict['train_combined_loss_per_batch'].append(loss.item())
+            log_dict['train_reconstruction_loss_per_batch'].append(pixelwise.item())
+            log_dict['train_kl_loss_per_batch'].append(kl_div.item())
+            
+            if not batch_idx % logging_interval:
+                print('Epoch: %03d/%03d | Batch %04d/%04d | Loss: %.4f'
+                      % (epoch+1, num_epochs, batch_idx,
+                          len(train_loader), loss))
+
+        if not skip_epoch_stats:
+            model.eval()
+            
+            with torch.set_grad_enabled(False):  # save memory during inference
+                
+                train_acc = compute_accuracy(model, train_loader, device)         # Add this
+                train_loss = compute_epoch_loss_autoencoder(
+                    model, train_loader, loss_fn, device)
+                print('***Epoch: %03d/%03d | Loss: %.3f' % (
+                      epoch+1, num_epochs, train_acc, train_loss))                # Add train_acc
+                log_dict['train_combined_per_epoch'].append(train_loss.item())
+                log_dict['train_acc_per_epoch'].append(train_acc.item())          # Add this
+                
+                # Add this (valid_loader)
+                if valid_loader is not None:
+                    valid_acc = compute_accuracy(model, valid_loader, device)
+                    valid_loss = compute_epoch_loss_classifier(
+                        model, valid_loader, loss_fn, device)
+                    print('***Epoch: %03d/%03d | Valid. Acc.: %.3f%% | Loss: %.3f' % (
+                          epoch+1, num_epochs, valid_acc, valid_loss))
+                    log_dict['valid_loss_per_epoch'].append(valid_loss.item())
+                    log_dict['valid_acc_per_epoch'].append(valid_acc.item())
+
+        print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
+    
+    print('Total Training Time: %.2f min' % ((time.time() - start_time)/60))
+    if save_model is not None:
+        torch.save(model.state_dict(), save_model)
+    
+    return log_dict
+
+
+#Old
 def train_vae_v1(num_epochs, model, optimizer, device, 
                  train_loader, loss_fn=None,
                  logging_interval=100, 
